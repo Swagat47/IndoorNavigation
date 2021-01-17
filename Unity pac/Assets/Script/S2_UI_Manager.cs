6317b575e05679f8f9310de5ce9a52221c0aff8e
Mihir0106 <mjmihir9617@gmail.com>
2021-01-17 21:27:16 +0530

    updated Unity package

---
+++ Unity pac/Assets/Script/S2_UI_Manager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class S2_UI_Manager : MonoBehaviour
+{ 
+    /*
+    [SerializeField]
+    [Tooltip("The ARcameraManager which will produce frame events.")]
+    ARCameraManager m_CameraManager;
+    
+    /// <summary>
+    /// Get or set the <c>ARCameraManager</c>.
+    /// </summary>
+    public ARCameraManager cameraManager
+    {
+        get { return m_CameraManager; }
+        set
+        {
+            if (m_CameraManager == value)
+                return;
+
+            if (m_CameraManager != null)
+                m_CameraManager.frameReceived -= FrameChanged;
+
+            m_CameraManager = value;
+
+            if (m_CameraManager != null & enabled)
+                m_CameraManager.frameReceived += FrameChanged;
+        }
+    }
+
+    const string k_FadeOffAnim = "FadeOff";
+    const string k_FadeOnAnim = "FadeOn";
+
+    [SerializeField]
+    ARPlaneManager m_PlaneManager;
+
+    public ARPlaneManager planeManager
+    {
+        get { return m_PlaneManager; }
+        set { m_PlaneManager = value; }
+    }
+    */
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
